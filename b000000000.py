0614dd554eeaf9340bcdc49d7bf076dee148532b
didier300110500 <guigma_d@yahoo.fr>
2020-04-14 11:22:00 -0400

    .

--- b000000000.py
+++ b000000000.py
@@ -20,7 +20,55 @@ session = mysqlx.get_session({
 
 db = session.get_schema("world_x")
 
+
+def lecture(fichier):
+
+  # Le nom de la collection temporaire
+  nomColl = "temp"
+
+  # Crée la collection temporaire maColl
+  maColl = db.create_collection(nomColl)
+
+  # charge le fichier dans la variable json
+  json = charge(fichier)
+    
+  # Ajoute le contenu du fichier json dans la collection temporaire maColl
+  maColl.add(json).execute()
+
+  # Lis toute les documents convertis de la collection et les stocker dans la variable docs
+  docs = maColl.find().execute()
+
+  # Détruit la collection temporaire
+  db.drop_collection(nomColl)
+
+  # Retourne un dictionnaire Python du fichier json converti
+  return docs
+
+def former_des_chefs(docs):
+
+  # Crée une nouvelle collection 'chefs_de_gouvernement'
+  nomColl = 'chefs_de_gouvernement'
+  maColl = db.create_collection(nomColl)
+
+  # Manipuler la collection et la rajouter à la nouvelle
+  for doc in docs.fetch_all():
+    for country in doc.countries:
+      # Insert des documents JSON de type government
+      maColl.add(country['government']).execute()
+
+  # Trouver tous les documents JSON et les mettre en mémoire
+  docs = maColl.find().execute()
+
+  # Détruit la collection
+  db.drop_collection(nomColl)
+
+  return docs
+
 def main():
+
+  docs = lecture('b000000000.json')
+  chefs = former_des_chefs(docs)
+  print(len(chefs.fetch_all()))
   print(charge('b000000000.json'))
 
 # Ne pas oublier de remercier le gestionnaire de BD
@@ -28,3 +76,6 @@ def main():
 
 if __name__== "__main__":
     main()
+
+
+
