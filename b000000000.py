a84512a157300791dab6c50c8b95acfd62a8f5e8
didier300110500 <guigma_d@yahoo.fr>
2020-04-14 10:52:28 -0400

    .

--- b000000000.py
+++ b000000000.py
@@ -10,8 +10,21 @@ def charge(fichier):
    with open(fichier) as f:
       return json.load(f)
 
+import mysqlx
+session = mysqlx.get_session({
+    "host": "localhost",
+    "port": 33060,
+    "user": "root",
+    "password": "password"
+})
+
+db = session.get_schema("world_x")
+
 def main():
   print(charge('b000000000.json'))
+
+# Ne pas oublier de remercier le gestionnaire de BD
+  session.close
 
 if __name__== "__main__":
     main()
